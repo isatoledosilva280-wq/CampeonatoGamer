0382bb3c5fdb573837ce009eecf2668ced1bc390
ISADORA SILVA DE TOLEDO <26171733@edu119.local>
2026-09-24 16:26:41 -0300

    Atualizando

--- server.js
+++ server.js
@@ -45,6 +45,61 @@ app.get("/partidas/:id", (req, res) => {
     res.status(200).json(partida);
 });
 
+app.post("/partidas",(req, res)=>{
+    const {jogo, temeA, temB} = req.body;
+
+    if(!jogo || !timeA || !timeB){
+        return res.status(400).json({
+            mensagem: "Informe jogo, timeA e timeB"
+        });
+    }
+
+    if(timeA.trim().toLowerCase() === timeB.trim().toLowerCase()){
+         return res.status(400).json({
+            mensagem: "OS time devem ser diferentes"
+        });
+    }
+
+    const novoId = PARTIDAS.length > 0
+        ?Math.max(...PARTIDAS.map(p => p.id)) +1 :1;
+
+    const novaPartida ={
+        id: novoId,
+        jogo: jogo.trim(),
+        timeA: timeA.trim(),
+        timeB: timeB.trim(),
+        pontoA: 0,
+        pontoB: 0,
+        status: "agendada",
+    };
+
+    PARTIDAS.push(novaPartida);
+    res.status(201).json({
+        mensagem: "Partida Cadastrada",
+        partida: novaPartida
+    });
+
+});
+
+app.put("/partidas/:id", (req, res)=>{
+    const id = Number(req.params.id);
+    const indice = PARTIDAS.findIndex(p => p.id === id);
+
+    if(indice === -1){
+        return res.status(404).json({mensagem: "Partida não encontrada"})
+    }
+    const{jogo, timeA, timeB, pontoA, pontoB, status} = req.body;
+
+    if(!jogo || !timeA || timeB 
+        || !Number.isInteger(pontoA)
+        || Number.isInteger(pontoB)
+        || pontoA < 0 || pontoB <0
+        || ["agendada", "finalizada"].includes(status)
+    ){
+        
+    }
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
